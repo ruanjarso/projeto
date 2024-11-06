a2a432179ba6c7fed6411e044569f4bd3b0c2712
Ruan Jarso <105135491+ruanjarso@users.noreply.github.com>
2024-11-06 00:14:20 -0300

    add coment

--- ex001.py
+++ ex001.py
@@ -1,1 +1,5 @@
 print("Hello, World!")
+
+#Seguinte Galera, esse arquivo de Python vai pro beleleu depois,
+#foi só um arquivo que criei pro repositórtio não ficar em branco!
+#A ideia é que venhamos decidir antes o que vamos fazer e só depois começar a codar!
